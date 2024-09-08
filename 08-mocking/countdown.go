d42273a96b5a8fe85fbb3eadeebd52ad9b275b0a
Hideyuki Kanazawa <kanazawahideyuki@gmail.com>
2024-09-08 18:09:12 +0800

    Adding mocking, conc, select, reflection

---
+++ 08-mocking/countdown.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const write = "write"
+const sleep = "sleep"
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, count int, sleeper Sleeper) {
+	for i := count; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := DefaultSleeper{}
+	Countdown(os.Stdout, 3, &sleeper)
+}
---
+++ 11-reflection/README.md
@@ -0,0 +1,6 @@
+> golang challenge: write a function walk(x interface{}, fn func(string)) which takes a struct x and calls fn for all strings fields found inside. difficulty level: recursively.
+
+
+Reflection in computing is the ability of a program to examine its own structure, particularly through types; its a form of metaprogramming, also a great source of confusion.
+
+
